8142afce55c671fc17ad72ac80187bc3bc01832e
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-11-29 21:07:10 +0100

    first commit

---
+++ day01/day01.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	var a, b int = -1, -1
+
+	day := "day01"
+	buf, _ := ioutil.ReadFile("_input.txt")
+	lines := strings.Split(string(buf), "\r\n")
+
+	start := time.Now()
+	for _, line := range lines {
+		fmt.Println(line)
+	}
+	elapsed := time.Since(start)
+
+	fmt.Printf("%v, a: %v, b: %v, time: %v", day, a, b, elapsed)
+
+}
+
+func solve(input []int) (int, int) {
+	answerA, answerB := -1, -1
+	return answerA, answerB
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bfv/aoc2021-go
+
+go 1.17
+
+require github.com/bfv/aoc2020-go v0.0.0-20210913181946-8e019e020a3e // indirect
