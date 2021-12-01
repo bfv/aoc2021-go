5e2cdf2e5437789ad16beea8f7662b8322552fbe
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-12-01 12:02:05 +0100

    day01

--- day01/day01.go
+++ day01/day01.go
@@ -5,27 +5,40 @@ import (
 	"io/ioutil"
 	"strings"
 	"time"
+
+	"github.com/bfv/aoc2021-go/aoc"
 )
 
 func main() {
 
+	start := time.Now()
+
 	var a, b int = -1, -1
 
 	day := "day01"
-	buf, _ := ioutil.ReadFile("_input.txt")
-	lines := strings.Split(string(buf), "\r\n")
+	buf, _ := ioutil.ReadFile("input.txt")
+	input := strings.Split(string(buf), "\r\n")
 
-	start := time.Now()
-	for _, line := range lines {
-		fmt.Println(line)
+	prev, prevB := -1, -1
+
+	ints := aoc.GetIntArray(input)
+	for i, x := range ints {
+		// part a
+		if x > prev {
+			a++
+		}
+		prev = x
+
+		// part b
+		if i > 1 {
+			y := ints[i-2] + ints[i-1] + ints[i]
+			if y > prevB {
+				b++
+			}
+			prevB = y
+		}
 	}
+
 	elapsed := time.Since(start)
-
 	fmt.Printf("%v, a: %v, b: %v, time: %v", day, a, b, elapsed)
-
-}
-
-func solve(input []int) (int, int) {
-	answerA, answerB := -1, -1
-	return answerA, answerB
 }
