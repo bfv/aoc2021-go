4bf71753d19ad197d7dbf4988ed681445ad1c088
Bronco Oostermeyer <dev@bfv.io>
2021-12-02 05:29:54 +0000

    day02

--- day02/day02.go
+++ day02/day02.go
@@ -1,11 +1,25 @@
-package aoc
+package main
 
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 	"time"
 )
+
+type movement struct {
+	direction string
+	amount    int
+}
+
+func (m movement) parse(line string) movement {
+	parts := strings.Split(line, " ")
+	m1 := movement{}
+	m1.direction = parts[0]
+	m1.amount, _ = strconv.Atoi(parts[1])
+	return m1
+}
 
 func main() {
 
@@ -13,16 +27,57 @@ func main() {
 
 	var a, b int = 0, 0
 
-	day := "day01"
+	day := "day02"
 	buf, _ := ioutil.ReadFile("input.txt")
-	input := strings.Split(string(buf), "\r\n")
+	input := strings.Split(string(buf), "\n")
+	movements := processInput(input)
 
-	
+	a = solveA(movements)
+	b = solveB(movements)
+
 	elapsed := time.Since(start)
 	fmt.Printf("%v, a: %v, b: %v, time: %v", day, a, b, elapsed)
 }
 
-func solve(input []int) (int, int) {
-	answerA, answerB := -1, -1
-	return answerA, answerB
+func solveA(ms []movement) int {
+	var x, depth = 0, 0
+	for _, m := range ms {
+		switch m.direction {
+		case "forward":
+			x += m.amount
+		case "down":
+			depth += m.amount
+		case "up":
+			depth -= m.amount
+		}
+	}
+	return x * depth
+}
+
+func solveB(ms []movement) int {
+
+	var x, depth, aim = 0, 0, 0
+	for _, m := range ms {
+		switch m.direction {
+		case "forward":
+			x += m.amount
+			depth += (aim * m.amount)
+		case "down":
+			aim += m.amount
+		case "up":
+			aim -= m.amount
+
+		}
+	}
+	return x * depth
+}
+
+func processInput(lines []string) []movement {
+	var m movement
+	ms := []movement{}
+	for _, line := range lines {
+		m := m.parse(line)
+		ms = append(ms, m)
+	}
+	return ms
 }
