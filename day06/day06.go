eade2e4b4b055e309e17192cd299940323c95e09
Bronco Oostermeyer <b.oostermeyer@flusso.nl>
2021-12-06 10:24:04 +0100

    refactored A to solution of B

--- day06/day06.go
+++ day06/day06.go
@@ -9,68 +9,26 @@ import (
 	"github.com/bfv/aoc2021-go/lib"
 )
 
-type Fish struct {
-	timer int
-}
-
-func (f *Fish) addDay() bool {
-	createNew := false
-	if f.timer == 0 {
-		f.timer = 7
-		createNew = true
-	}
-	f.timer--
-	return createNew
-}
-
-var school []Fish
 var timings []int
 
 func main() {
 
 	start := time.Now()
 
-	var a, b int = 0, 0
-
 	day := "day06"
-	input := aoc.GetStringArray("input.txt")[0]
-	inputStrings := strings.Split(input, ",")
-	for _, t := range inputStrings {
-		school = append(school, Fish{lib.Atoi(t)})
-	}
+	inputStrings := strings.Split(aoc.GetStringArray("input.txt")[0], ",")
 
-	a = solveA()
+	timings = getTiming(inputStrings)
+	a := solve(80)
 
-	timings = make([]int, 9)
-	for _, f := range inputStrings {
-		timings[lib.Atoi(f)]++
-	}
-	b = solveB(256)
+	timings = getTiming(inputStrings)
+	b := solve(256)
 
 	elapsed := time.Since(start)
 	fmt.Printf("%v, a: %v, b: %v, time: %v", day, a, b, elapsed)
 }
 
-func solveA() int {
-
-	answerA := 0
-	newSchool := school
-	for day := 1; day <= 80; day++ {
-		tmp := newSchool
-		for i, f := range tmp {
-			createNew := f.addDay()
-			tmp[i] = f
-			newSchool[i] = f
-			if createNew {
-				newSchool = append(newSchool, Fish{8})
-			}
-		}
-	}
-	answerA = len(newSchool)
-	return answerA
-}
-
-func solveB(days int) int {
+func solve(days int) int {
 	for day := 1; day <= days; day++ {
 		shift()
 	}
@@ -84,4 +42,12 @@ func shift() {
 	}
 	timings[6] += new
 	timings[8] = new
+}
+
+func getTiming(inputStrings []string) []int {
+	timings = make([]int, 9)
+	for _, f := range inputStrings {
+		timings[lib.Atoi(f)]++
+	}
+	return timings
 }
